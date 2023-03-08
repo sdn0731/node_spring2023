605db1976a7b8e4d017a409fd86480feceb2a4c8
Sarah "Dasha" Nolan <snolan0731@gmail.com>
2023-03-08 23:17:17 +0000

    create and connect database

--- app.js
+++ app.js
@@ -1,15 +1,23 @@
 var express = require('express');
+var mongoose = require('mongoose')
 var app = express();
 
-app.use('/static', express.static("public"))
+app.use('/static', express.static("public"));
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 
-app.get('/hello', function(req, res){
-    res.send('Hello World');
-})
+const mongoDB = 'mongodb+srv://sarah:LLm0iVxIaJocO7nL@cluster0.hrmd2q7.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+let db = mongoose.connection;
+db.on('error', console.error.bind(console, "MongoDB connection error: "))
 
 app.get('/', function(req, res){
     res.render('todo.ejs');
+})
+
+app.post('/', (req, res) => {
+    console.log(req.body.content)
 })
 
 app.listen(3000, function(){
