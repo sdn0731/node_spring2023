9b9873c0dfeb15621bd584dc56ba0074146417a9
Sarah "Dasha" Nolan <snolan0731@gmail.com>
2023-03-14 20:46:32 +0000

    added axios and comic

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var mongoose = require('mongoose')
 var app = express();
-const axios = require('axios').default;
 const bodyParser = require('body-parser');
 
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -10,6 +9,7 @@ app.use(bodyParser.json());
 app.use('/static', express.static("public"));
 app.set("view engine", "ejs");
 const Todo = require('./models/todo.model');
+const { default: axios } = require('axios');
 const mongoDB = 'mongodb+srv://sdn731:a8trXXXxcZGqsFwD@cluster0.hrmd2q7.mongodb.net/?retryWrites=true&w=majority'
 mongoose.connect(mongoDB);
 mongoose.Promise = global.Promise;
@@ -17,13 +17,17 @@ let db = mongoose.connection;
 db.on('error', console.error.bind(console, "MongoDb connection error: "))
 
 app.get('/', function(req, res){
-    Todo.find(function(err, todo){
-        console.log(todo)
-        if(err){
-            res.json({"Error: ": err})
-        } else {
-            res.render('todo.ejs', {todoList: todo});
-        }
+    axios.get('https://xkcd.com/info.0.json').then(function(response){
+        Todo.find(function(err, todo){
+            console.log(todo)
+            if(err){
+                res.json({"Error: ": err})
+            } else {
+                res.render('todo.ejs', {todoList: todo, comicData: response.data});
+            }
+        })
+    }).catch(function(error){
+        res.json({"Error: ": error})
     })
 })
 
